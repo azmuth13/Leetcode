c38f8ec455d6c59e49cef6b340ba13f14add80dd
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-02-14 21:26:14 +0530

    Time: 164 ms (62.60%), Space: 71.9 MB (47.76%) - LeetHub

--- 687-longest-univalue-path/687-longest-univalue-path.cpp
+++ 687-longest-univalue-path/687-longest-univalue-path.cpp
@@ -21,31 +21,38 @@ public:
 //     }
     
     int HH = 0;
-        
-    map <TreeNode* , int> mp;
-    
-    
+
     int helper(TreeNode* root, int val)
     {
         if(!root)
             return 0;
         
-        if(root->val == val)
-        {
-            int l = helper(root->left, val);
-            int r = helper(root->right, val);
-            HH = max(HH, l+r+1);
-            return 1 + max(l,r);
-        }
-        else
-        {
-            int l = helper(root->left, root->val);
-            int r = helper(root->right, root->val);
+//         if(root->val == val)
+//         {
+//             int l = helper(root->left, val);
+//             int r = helper(root->right, val);
+//             HH = max(HH, l+r+1);
+//             return 1 + max(l,r);
+//         }
+//         else
+//         {
+//             int l = helper(root->left, root->val);
+//             int r = helper(root->right, root->val);
             
-            HH = max(HH, l+r+1);
+//             HH = max(HH, l+r+1);
             
+//             return 0;
+//         }
+        
+        int l = helper(root->left, root->val);
+        int r = helper(root->right, root->val);
+        
+        HH = max(HH, l+r+1);
+        
+        if(root->val != val)
             return 0;
-        }
+        
+        return 1 + max(l,r);
     }
     
     int longestUnivaluePath(TreeNode* root) {
