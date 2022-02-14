c288e2d959b6f19ced5393344f1d878ab7683259
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-02-14 19:07:08 +0530

    Time: 16 ms (9.55%), Space: 18.4 MB (6.16%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,110 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+//     TreeNode* res;
+//     map <int, int> mp;
+//     TreeNode* helper(vector <int> &pre, vector <int> &ino, int i, int l, int r)
+//     {
+//         if(l >= r)
+//             return NULL;
+        
+        
+//         int ind = mp[pre[i]];
+//         TreeNode* root = new TreeNode(pre[i]);
+        
+//         root->left = helper(pre, ino, l, l, ind-1);
+//         root->right = helper(pre, ino, ind+1, ind+1, r);
+        
+//         return root;
+//     }
+    
+    
+    TreeNode* solve(vector <int> &pre, vector <int> &ino, int i)
+    {
+        
+        if(i == -1)
+            return NULL;
+        
+        int ind = 0;
+        for(int j = 0; j < ino.size(); j++)
+        {
+            if(ino[j]==pre[i])
+            {
+                ind = j;
+                break;
+            }
+        }
+            
+        vector <int> left, right;
+        
+        for(int j = 0; j < ino.size(); j++)
+        {
+            if(j < ind)
+            {
+                left.push_back(ino[j]);
+            }
+            else if(j > ind)
+            {
+                right.push_back(ino[j]);
+            }
+        }
+        
+       
+        map <int, int> f,g;
+        for(auto x : right)
+            f[x]++;
+        
+        for(auto x : left)
+            g[x]++;
+        
+        int rightStart = -1;
+        for(int k = 0; k < pre.size(); k++)
+        {
+            if(f.count(pre[k]))
+            {
+                rightStart = k;
+                break;
+            }
+        }
+        
+        int leftStart = -1;
+        for(int k = 0; k < pre.size(); k++)
+        {
+            if(g.count(pre[k]))
+            {
+                leftStart = k;
+                break;
+            }
+        }
+        
+        TreeNode* root = new TreeNode(pre[i]);
+        
+        root->left = solve(pre, left, leftStart);
+        root->right = solve(pre, right, rightStart);
+        
+        return root;
+        
+    }
+    
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        
+        vector <int> inorder = preorder;
+        sort(inorder.begin(), inorder.end());
+        int n = preorder.size();
+      
+        TreeNode* ans = solve(preorder, inorder, 0);
+        return ans;
+       
+    }
+};
