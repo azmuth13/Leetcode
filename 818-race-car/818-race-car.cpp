19cd74bc23b1d8b9ad689c420da155b8233a95bd
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-02-28 18:41:50 +0530

    Time: 1641 ms (6.43%), Space: 105.3 MB (13.57%) - LeetHub

---
+++ 818-race-car/818-race-car.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    
+    int racecar(int target) {
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        
+        int ans = 0;
+        
+        queue < pair <int, int> > q;
+        //set < pair <ll, ll> > vis;
+        
+        //unordered_map < ll, unordered_map <ll, bool> > vis;
+        
+        unordered_map <string, int> vis;
+        
+        q.push(make_pair(0,1));
+            
+        string key = to_string(0) + " " + to_string(1);
+        vis[key] = 1;
+        
+        while(!q.empty())
+        {
+            int sz = q.size();
+            
+            while(sz--)
+            {
+                pair <int, int> pp = q.front();
+                q.pop();
+                 
+                if(pp.first == target)
+                {
+                    return ans;
+                }
+                
+                // instruction A
+                
+                if(pp.first + pp.second > 0 && pp.first + pp.second < 2*target)
+                {
+                    string key = to_string(pp.first + pp.second) + " " + to_string(2*pp.second);
+                    if(!vis.count(key))
+                    {
+                        q.push(make_pair(pp.first + pp.second, pp.second*2));
+                        vis[key] = 1;
+                    }
+                }
+                
+                
+                // instruction R
+                
+                if(pp.second >= 0)
+                {
+                    string key = to_string(pp.first) + " " + to_string(-1);
+                    if(!vis[key])
+                    {
+                        q.push(make_pair(pp.first, -1));
+                        vis[key] = 1;
+                    }
+                   
+                }
+                else
+                {
+                    string key = to_string(pp.first) + " " + to_string(1);
+                    if(!vis[key])
+                    {
+                        q.push(make_pair(pp.first, 1));
+                        vis[key] = 1;
+                    }
+                    
+                }
+            }
+            
+            ans++;
+        }
+        
+        return 0;
+    }
+};
