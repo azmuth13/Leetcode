30d47b00a2bac21981eed81cad8a69d443ad4a5c
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-03-07 10:02:44 +0530

    Time: 414 ms (41.18%), Space: 127.2 MB (94.12%) - LeetHub

---
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    using ll = long long;
+    
+    ll LCM(ll a, ll b)
+    {
+        return (a*b)/(__gcd(a,b));
+    }
+    
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> ans;
+        
+        stack <ll> st;
+ 
+        
+        for(int i = 0; i < n; i++)
+        {
+            
+            if(st.size() > 0 && __gcd((ll)st.top(), (ll)nums[i]) > 1ll)
+            {
+                ll val = nums[i];
+                while(st.size() > 0 && __gcd((ll)st.top(), val) > 1ll)
+                {
+                   // cout << st.top() << " " << nums[i] << endl;
+                    
+                    val = LCM(val, st.top());
+                    st.pop();
+                }
+                st.push(val);
+            }
+            else
+            {
+                st.push(nums[i]);
+            }
+            
+          
+            
+        }
+        
+        while(st.size() > 0)
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+//         nums = ans;
+//         while(st.size() > 0)
+//             st.pop();
+//         st.push(nums[0]);
+        
+//         for(int i = 1; i < n; i++)
+//         {
+            
+//             if(__gcd((ll)st.top(), (ll)nums[i]) > 1)
+//             {
+//                 cout << st.top() << " " << nums[i] << endl;
+//                 int lcm = LCM(st.top(), nums[i]);
+//                 st.pop();
+//                 st.push(lcm);
+//             }
+//             else
+//             {
+//                 st.push(nums[i]);
+//             }
+//         }
+//         ans.clear();
+//         while(st.size() > 0)
+//         {
+//             ans.push_back(st.top());
+//             st.pop();
+//         }
+//        reverse(ans.begin(), ans.end());
+         return ans;
+    }
+};
