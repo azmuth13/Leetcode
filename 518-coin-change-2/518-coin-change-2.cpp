b19615c7966e9f5dcb431ecdaee79359d5839588
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-03-10 21:35:19 +0530

    Time: 37 ms (43.54%), Space: 13.9 MB (42.44%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int memo[301][5001];
+    int helper(vector <int> &coins, int n, int x, int i)
+    {
+        if(x == 0) return 1;
+
+        if(i >= n || x < 0) return 0;
+        
+        if(memo[i][x] != -1)
+            return memo[i][x];
+        
+        int ans1 = 0, ans2 = 0;
+        ans1 = helper(coins, n, x - coins[i], i);
+        ans2 = helper(coins, n, x, i+1);
+        return memo[i][x] = (ans1 + ans2);
+    }
+
+    
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        
+        for(int i = 0; i <= n; i++)
+        {
+            for(int j = 0; j <= amount; j++)
+                memo[i][j] = -1;
+        }
+        int res = helper(coins, n, amount, 0);
+        return res;
+        
+        int dp[5001] = {};
+        
+        
+        dp[0] = 1;
+        
+        for(int currSum = 0; currSum <= amount; currSum++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(coins[j] <= currSum)
+                {
+                    dp[currSum] += dp[currSum - coins[j]];
+                }
+            }
+        }
+        
+        return dp[amount];
+    }
+};
