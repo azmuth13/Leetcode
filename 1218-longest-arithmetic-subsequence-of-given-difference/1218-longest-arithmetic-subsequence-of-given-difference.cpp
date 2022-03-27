acdbe9ab41a0affd47f6f42d3a3d9cc69485f246
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-03-27 12:53:34 +0530

    Time: 261 ms (33.98%), Space: 83.2 MB (5.02%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
+    
+    unordered_map <int, int> mp;
+    
+    int helper(vector <int> &arr, int d, int i)
+    {
+        if(i == arr.size())
+        {
+            return 0;
+        }
+        int ans = 1;
+        if(mp.find(arr[i] - d) != mp.end())
+        {
+            mp[arr[i]] = max(mp[arr[i]], 1 + mp[arr[i] - d]);
+            ans = max(ans, mp[arr[i]]);
+        }
+        mp[arr[i]] = max(mp[arr[i]], 1);
+        return max(ans, helper(arr, d, i+1));
+    }
+    
+    
     int longestSubsequence(vector<int>& arr, int d) {
         
         int n = arr.size();
         
-        unordered_map <int, int> mp;
+//         unordered_map <int, int> mp;
 
-        int ans = 1;
-        for(int i = 0; i < n; i++)
-        {
-            if(mp.find(arr[i] - d) != mp.end())
-            {
-                mp[arr[i]] = max(mp[arr[i]], 1 + mp[arr[i] - d]);
-                ans = max(ans, mp[arr[i]]);
-            }
-            mp[arr[i]] = max(mp[arr[i]], 1);
-        }
+//         int ans = 1;
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(mp.find(arr[i] - d) != mp.end())
+//             {
+//                 mp[arr[i]] = max(mp[arr[i]], 1 + mp[arr[i] - d]);
+//                 ans = max(ans, mp[arr[i]]);
+//             }
+//             mp[arr[i]] = max(mp[arr[i]], 1);
+//         }
         
+//         return ans;
+        
+        int ans = helper(arr, d, 0);
         return ans;
     }
 };
