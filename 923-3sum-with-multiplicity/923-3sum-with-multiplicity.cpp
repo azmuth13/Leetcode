58ad55301b955b84bbc60c814230a8c17272c442
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-04-07 09:21:53 +0530

    Time: 3 ms (99.66%), Space: 10.5 MB (62.54%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,20 +1,76 @@
 class Solution {
 public:
-        int threeSumMulti(vector<int>& A, int target) {
-        unordered_map<int, long> c;
-        for (int a : A) c[a]++;
-        long res = 0;
-        for (auto it : c)
-            for (auto it2 : c) {
-                int i = it.first, j = it2.first, k = target - i - j;
-                if (!c.count(k)) continue;
-                if (i == j && j == k)
-                    res += c[i] * (c[i] - 1) * (c[i] - 2) / 6;
-                else if (i == j && j != k)
-                    res += c[i] * (c[i] - 1) / 2 * c[k];
-                else if (i < j && j < k)
-                    res += c[i] * c[j] * c[k];
+    int threeSumMulti(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        long long mod = 1e9 + 7;
+        
+        if(n==0)
+            return 0;
+        long long ans = 0;
+        
+        sort(nums.begin(), nums.end());
+        
+        long long count[101];
+        
+        for(int i = 0; i < 101 ;i++)
+            count[i] = 0;
+        
+        for(int i = 0; i < n; i++)
+            count[nums[i]]++;
+        
+        vector <long long> uniq;
+        
+        for(int i = 0; i <= 100; i++)
+        {
+            if(count[i] > 0)
+                uniq.push_back(i);
+        }
+        // for(auto x : uniq)
+        //     cout << x << " ";
+        // cout << endl;
+        int sz = uniq.size();
+        
+        for(int i=0; i<sz; ++i)
+        {
+            
+            int j = i;
+            int k = sz-1;
+            
+            while(j <= k)
+            {
+                if(uniq[j] + uniq[k] + uniq[i] > target)
+                {
+                    k--;
+                }
+                else if(uniq[j] + uniq[k] + uniq[i] < target)
+                {
+                    j++;
+                }
+                else
+                {
+                    if(i < j && j < k)
+                    {
+                        ans += count[uniq[i]] * count[uniq[j]] * count[uniq[k]];
+                    }
+                    else if(i == j && j < k)
+                    {
+                        ans += (count[uniq[i]] * (count[uniq[i]]-1))/2 * count[uniq[k]];
+                    }
+                    else if(i < j && j == k)
+                    {
+                        ans += (count[uniq[j]] * (count[uniq[j]]-1))/2 * count[uniq[i]];
+                    }
+                    else
+                    {
+                        ans += (count[uniq[j]] * (count[uniq[j]]-1) * (count[uniq[j]]-2))/6;
+                    }
+                    ans %= mod;
+                    j++, k--;
+                }   
             }
-        return res % int(1e9 + 7);
+        }
+        
+        return ans;
     }
 };
