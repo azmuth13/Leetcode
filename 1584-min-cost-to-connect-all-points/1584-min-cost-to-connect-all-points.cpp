764369fe66eef4e79fa5c522d1b345c34e391c1a
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-04-22 16:01:59 +0530

    Time: 614 ms (51.37%), Space: 101.6 MB (36.07%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -10,9 +10,7 @@ public:
         {
             for(int j = i+1; j < n; j++)
             {
-                if(i == j)
-                    continue;
-                
+    
                 int wt = abs(points[i][0] - points[j][0]) + 
                     abs(points[i][1] - points[j][1]);
                 
@@ -32,31 +30,59 @@ public:
             parent[i] = -1;
         }
         
-        key[0] = 0;
+         key[0] = 0;
         
-        for(int count = 0; count < n-1; count++)
-        {
-            int mini = INT_MAX, u;
+//         for(int count = 0; count < n-1; count++)
+//         {
+//             int mini = INT_MAX, u;
             
-            for(int v = 0; v < n; v++)
-            {
-                if(mstSet[v] == false && key[v] < mini)
-                {
-                    mini = key[v], u = v;
-                }
-            }
+//             for(int v = 0; v < n; v++)
+//             {
+//                 if(mstSet[v] == false && key[v] < mini)
+//                 {
+//                     mini = key[v], u = v;
+//                 }
+//             }
+            
+//             mstSet[u] = true;
+            
+//             for(auto it : adj[u])
+//             {
+//                 int v = it.first;
+//                 int wt = it.second;
+                
+//                 if(mstSet[v] == false && wt < key[v])
+//                     parent[v] = u, key[v] = wt;
+//             }
+//         }
+        priority_queue < pair <int, int>, vector < pair <int, int> >, greater <pair <int, int> > > pq;
+
+        pq.push({0, 0});
+        
+        //for(int count = 0; count < n-1; count++)
+        while(!pq.empty())
+        {
+            int u = pq.top().second;
+            
+            
+            pq.pop();
             
             mstSet[u] = true;
-            
+
             for(auto it : adj[u])
             {
                 int v = it.first;
                 int wt = it.second;
-                
+
                 if(mstSet[v] == false && wt < key[v])
-                    parent[v] = u, key[v] = wt;
+                {
+                    parent[v] = u;
+                    key[v] = wt;
+                    pq.push({key[v], v});
+                }
             }
         }
+        
         int ans = 0;
         
         for(int i = 1; i < n; i++)
