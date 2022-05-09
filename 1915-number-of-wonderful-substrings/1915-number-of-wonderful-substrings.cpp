1ca610a9da8fe15ad7bf0712653ec6aa9c0819e3
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-05-09 10:39:46 +0530

    Time: 1367 ms (8.38%), Space: 20.1 MB (8.37%) - LeetHub

---
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    using ll = long long;
+    
+    long long wonderfulSubstrings(string word) {
+        
+        ll n = word.size();
+        ll ans = 0;
+        
+        ll hash = 0;
+        map <ll, ll> mp;
+        mp[0] = 1;
+        for(int i = 0; i < n; i++)
+        {
+            hash = hash ^ (1ll << (word[i] - 'a'));
+            
+            ans += mp[hash];
+            
+            for(int k = 0; k <= 10; k++)
+            {
+                ll nhash = hash ^ (1ll<<(k));
+                ans += mp[nhash];
+            }
+            
+            mp[hash]++;
+        }
+        return ans;
+    }
+};
