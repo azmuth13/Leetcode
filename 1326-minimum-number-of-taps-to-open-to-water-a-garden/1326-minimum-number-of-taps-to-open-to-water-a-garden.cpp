5180bc932fc360339ac17f6503e0c5828f729cae
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-05-14 17:13:24 +0530

    Time: 18 ms (80.91%), Space: 14.7 MB (64.71%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -2,36 +2,36 @@ class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
         
-        vector <int> R(n+1, 0);
+        vector <int> R(n+1,0);
+        
         for(int i = 0; i <= n; i++)
         {
             int l = i - ranges[i];
             int r = i + ranges[i];
+            
             if(l < 0)
                 l = 0;
             R[l] = max(R[l], r);
         }
         
+        int i=0, end = 0, farthest = 0;
         int ans = 0;
-        
-        int end = 0, farthest = 0;
-        
-        int i = 0;
-        
         while(i <= n && end < n)
         {
             ans++;
             
-            while(i < n && i <= end)
+            while(i <= end && end < n)
             {
                 farthest = max(farthest, R[i]);
                 i++;
             }
+            
             if(end == farthest)
                 return -1;
+            
             end = farthest;
+            
         }
         return ans;
-
     }
 };
