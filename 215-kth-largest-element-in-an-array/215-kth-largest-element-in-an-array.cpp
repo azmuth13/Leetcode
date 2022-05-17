6c80514a6c60ce16a691ce98ca77fac0392ce4d9
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-05-17 13:00:51 +0530

    Time: 12 ms (51.13%), Space: 10.1 MB (36.89%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,60 +1,106 @@
+
+class minheap{
+    
+    int a[100001];
+    int n=0;
+    public:
+    
+    minheap(){
+        
+    }
+    
+    void minHeapify(int a[], int n, int i)
+    {
+        int lindex = i;
+        int left = 2*i+1;
+        int right = 2*i + 2;
+        
+        if(left < n && a[left] < a[lindex])
+        lindex = left;
+        
+        if(right < n && a[right] < a[lindex])
+        lindex = right;
+        
+        if(lindex!=i)
+        {
+            // swapping
+            int temp = a[i];
+            a[i] = a[lindex];
+            a[lindex] = temp; 
+            
+            minHeapify(a,n,lindex);
+        }
+    }
+    
+    void buildHeap()
+    {
+        
+    }
+    
+    //---------------------//
+    
+    
+    void push(int num)
+    {   
+        a[n] = num;
+        int i = n;
+        
+        n++;
+        
+        int parent = (i-1)/2;
+        
+        while(i != 0 && a[parent] > a[i])
+        {
+            swap(a[parent], a[i]);
+            i = parent;
+            parent = (i-1)/2;
+        }
+    }
+    
+    int pop()
+    {
+        if(n == 0)
+        {
+            cout << "heap empty, returning -1";
+            return -1;
+        }
+        
+        int x = a[0];
+        swap(a[0], a[n-1]);
+        
+        n-=1;
+        
+        minHeapify(a, n, 0);
+        return x;
+    }
+    
+    int top()
+    {
+        if(n == 0)
+            return 1e9;
+        
+        return a[0];
+    }
+    
+    int size()
+    {
+        if(n<=0)
+            return 0;
+        
+        return n;
+    }
+};
+
+
 class Solution {
 public:
-    
-    int partition(int arr[], int l, int r)
-    {
-        int i = l + 1;
-        int piv = arr[l];
-        
-        for(int j = i; j <= r; j++)
-        {
-            if(arr[j] < piv)
-            {
-                swap(arr[j], arr[i]);
-                i++;
-            }
-        }
-        
-        swap(arr[i-1], arr[l]);
-        
-        return i-1;
-    }
-    
-    int rand_partition(int arr[], int start, int end)
-    {
-        int randomPivot = start + rand()%(end-start+1);
-    
-        swap(arr[start], arr[randomPivot]);
-    
-        return partition(arr, start, end);
-    }
-    
-    
-    int quick_sort(int arr[], int l, int r, int k)
-    {
-        int pivot = partition(arr, l, r);
-        
-        int count = r-pivot+1;
-        
-        if(count == k)
-        {
-            return arr[pivot];
-        }
-        else if(count > k)
-        {
-            return quick_sort(arr, pivot + 1, r, k); 
-        }
-        else
-        {
-             return quick_sort(arr, l, pivot - 1, k);
-        }
-    }
-    
-    
     int findKthLargest(vector<int>& nums, int k) {
         
-        priority_queue <int, vector <int> , greater <int> > pq;
-            
+//          min heap
+        //priority_queue <int, vector <int> , greater <int> > pq;
+        
+        minheap pq;
+        
         for(int i = 0; i < nums.size(); i++)
         {
             pq.push(nums[i]);
@@ -64,15 +110,6 @@ public:
         }
         
         return pq.top();
-        
-//         int arr[nums.size()];
-        
-//         for(int i = 0; i < nums.size(); i++)
-//             arr[i] = nums[i];
-        
-        
-        
-//         return quick_sort(arr, 0, nums.size()-1, k);
         
     }
 };
