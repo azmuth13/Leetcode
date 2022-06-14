7f9cacc7bd2a32ad3bd9ca3fe4f3594e940211fa
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-06-14 12:28:55 +0530

    Time: 476 ms (36.95%), Space: 6.6 MB (83.39%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -3,8 +3,10 @@ public:
     
     bool isPal(string &s, int i, int j)
     {
+        
         while(i < j)
         {
+            
             if(s[i] != s[j])
             {
                 return false;
@@ -14,6 +16,7 @@ public:
         }
         return true;
     }
+    
     
     int helper(string &s, int i, int *memo)
     {
@@ -44,6 +47,17 @@ public:
     
     int minCut(string s) {
         int n = s.size();
+        
+        
+        // bool ** storePal = new bool*[n+1];
+        // for(int i=0; i <= n; i++)
+        // {
+        //     storePal[i] = new bool[n+1];
+        //     for(int j = 0; j <= n; j++)
+        //     {
+        //         storePal[i][j] = 0;
+        //     }
+        // }
         
         if(isPal(s,0,n-1))
             return 0;
