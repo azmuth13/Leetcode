bbfd26544af283b0ed7fbcfb646720518323ae8a
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-06-14 12:19:20 +0530

    Time: 719 ms (13.35%), Space: 6.7 MB (77.60%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    
+    bool isPal(string &s, int i, int j)
+    {
+        while(i < j)
+        {
+            if(s[i] != s[j])
+            {
+                return false;
+            }
+            
+            i++,j--;
+        }
+        return true;
+    }
+    
+    int helper(string &s, int i, int *memo)
+    {
+        if(i >= s.size())
+            return 0;
+        
+        if(memo[i] != -1)
+            return memo[i];
+        
+        int ans = 1e9;
+        
+        for(int j = i; j < s.size(); j++)
+        {
+            if(isPal(s,i,j))
+            {
+                if(j != (s.size()-1))
+                {
+                    ans = min(ans, 1 + helper(s,j+1,memo));
+                }
+                else
+                {
+                    ans = min(ans, helper(s,j+1,memo));
+                }
+            }
+        }
+        return memo[i] = ans;
+    }
+    
+    int minCut(string s) {
+        int n = s.size();
+        
+        if(isPal(s,0,n-1))
+            return 0;
+        
+        int * memo = new int[n+1];
+        for(int i = 0; i <= n; i++)
+        {
+            memo[i] = -1;
+        }
+        
+        int ans = helper(s, 0, memo);
+        return ans;
+        
+    }
+};
