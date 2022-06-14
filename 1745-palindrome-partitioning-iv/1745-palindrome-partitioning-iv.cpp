e8ac0ced6395051a78930fbb670e3de8362dfc82
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-06-14 15:03:31 +0530

    Time: 388 ms (51.07%), Space: 201.4 MB (5.79%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -26,7 +26,7 @@ public:
     {
         if(k == 0)
         {
-            if(isPal(s,i,s.size()-1,pali))
+            if(pali[i][s.size()-1])
                 return true;
             
             return false;
@@ -42,7 +42,7 @@ public:
         
         for(int j = i; j < s.size()-1; j++)
         {
-            if(isPal(s,i,j,pali))
+            if(pali[i][j])
             {
                 ans = ans|helper(s,j+1, k-1, memo, pali);
                 if(ans)
@@ -57,25 +57,24 @@ public:
         int k = 2;
         int n = s.size();
         vector < vector <int> > memo(n+1,vector <int> (k+1,-1));
-        // int ** memo = new int*[n+1];
-        // for(int i = 0; i <= n; i++)
-        // {
-        //     memo[i] = new int[k+2];
-        //     for(int j = 0; j <= k+1; j++)
-        //     {
-        //         memo[i][j] = -1;
-        //     }
-        // }
-        vector < vector <int> > pali(n+1,vector <int> (n+1,-1));
-        // int ** pali = new int*[n+1];
-        // for(int i = 0; i <= n; i++)
-        // {
-        //     pali[i] = new int[n+1];
-        //     for(int j = 0; j <= n; j++)
-        //     {
-        //         pali[i][j] = -1;
-        //     }
-        // }
+
+        vector < vector <int> > pali(n+1,vector <int> (n+1,0));
+        
+        for(int i = n-1; i >= 0; i--)
+        {
+            for(int j = i; j < n; j++)
+            {
+                if(s[i] == s[j])
+                {
+                    pali[i][j] = i+1 >= j || pali[i+1][j-1];
+                }
+                else if(i == j)
+                {
+                    pali[i][j] = 1;
+                }
+            }
+        }
+
         int ans = helper(s, 0, k, memo, pali);
         return ans;
     }
