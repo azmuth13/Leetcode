8454e6c81bf04a7358d4fa08ee703a177ddae43f
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-06-18 01:48:01 +0530

    Time: 598 ms (5.18%), Space: 89 MB (5.03%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* store;
+    map < pair <ListNode*, TreeNode* >, bool > mp;
+    bool helper(ListNode* head, TreeNode* root)
+    {
+        if(head == NULL)
+            return true;
+        
+        if(!root)
+            return false;
+        
+        if(mp.find({head,root}) != mp.end())
+            return mp[{head,root}];
+        
+        bool ans = false;
+        
+        if(root->val == head->val)
+        {
+            ans |= (helper(head->next, root->left) || helper(head->next, root->right));
+        }
+        
+        ans |= (helper(store, root->left) || helper(store, root->right));
+        return mp[{head,root}] = ans;
+        
+    }
+    
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        store = head;
+        return helper(head,root);
+        
+    }
+};
