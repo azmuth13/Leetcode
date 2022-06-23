6d35636fd58cf1435052861ab9b2c766b71823fd
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-06-23 21:04:56 +0530

    Time: 15 ms (37.60%), Space: 7.4 MB (48.01%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,93 @@
+class Solution {
+public:
+    
+    bool hGood(vector <string> &board, int i, int j)
+    {
+        int n = board.size();
+        for(int k = 0; k < n; k++)
+        {
+            if(board[i][k] == 'Q')
+                return false;
+        }
+        return true;
+    }
+    
+    
+    bool vGood(vector <string> &board, int i, int j)
+    {
+        int n = board.size();
+        for(int k = 0; k < n; k++)
+        {
+            if(board[k][j] == 'Q')
+                return false;
+        }
+        return true;
+    }
+    
+    bool dsGood(vector <string> &board, int i, int j)
+    {
+        int n = board.size();
+        
+        int a = i-1, b = j-1;
+        while(a >= 0 && b >= 0)
+        {
+            if(board[a][b] == 'Q') return false;
+            a--,b--;
+        }
+        
+        a = i+1,b = j+1;
+        while(a < n && b < n)
+        {
+            if(board[a][b] == 'Q') return false;
+            a++,b++;
+        }
+        
+        
+        a = i-1,b= j+1;
+        while(a >= 0 && b < n)
+        {
+            if(board[a][b] == 'Q') return false;
+            a--,b++;
+        }
+        
+        a = i+1,b = j-1;
+        while(a < n && b >= 0)
+        {
+            if(board[a][b] == 'Q') return false;
+            a++,b--;
+        }
+        return true;
+    }
+    
+    
+    vector<vector<string>> result;
+    
+    void helper(int n, vector <string> &board, int i)
+    {
+        if(i == n)
+        {
+            result.push_back(board);
+            return;
+        }
+        for(int j = 0; j < n; j++)
+        {
+            if(board[i][j] == '.')
+            {
+                if(hGood(board,i,j) && vGood(board,i,j) && dsGood(board,i,j))
+                {
+                    board[i][j] = 'Q';
+                    helper(n,board,i+1);
+                    board[i][j] = '.';
+                }
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        
+        vector <string> board(n, string(n,'.'));
+        helper(n,board,0);
+        
+        return result;
+    }
+};
