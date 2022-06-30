38558506bc74148f208435a6006fd5a8cf85d352
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-06-30 14:42:57 +0530

    Time: 13 ms (45.45%), Space: 6.2 MB (25.00%) - LeetHub

---
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int memo[10][2][2][1024];
+    int helper(string &s, int i, int f, int c, int mask)
+    {
+        if(i >= s.size())
+        {
+            if(c)
+                return 1;
+            return 0;
+        }
+        if(memo[i][f][c][mask] != -1) return memo[i][f][c][mask];
+        int up = s[i] - '0';
+        int ans = 0;
+        
+        if(f)
+        {
+            up = 9;
+        }
+        
+        if(!c)
+        {
+            ans += helper(s,i+1,1,c,mask);
+            
+            for(int j = 1; j <= up; j++)
+            {
+                if(mask&(1<<j)) continue;
+                ans += helper(s,i+1,f||(j < up),1,mask|(1<<j));
+            }
+        }
+        else
+        {
+            for(int j = 0; j <= up; j++)
+            {
+                if(mask&(1<<j)) continue;
+                ans += helper(s,i+1,f||(j < up),1,mask|(1<<j));
+            }
+        }
+        return memo[i][f][c][mask] = ans;
+    }
+    
+    int numDupDigitsAtMostN(int n) {
+        
+        string s = to_string(n);
+        int mask = 0;
+        memset(memo,-1,sizeof memo);
+        int ans = helper(s,0,0,0,mask);
+        return n - ans;
+    }
+};
