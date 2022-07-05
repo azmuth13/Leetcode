d9fa1473c1334ab725252fd63333f76bdd3f68ad
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-07-05 21:34:43 +0530

    Time: 31 ms (99.43%), Space: 8.5 MB (88.34%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int n;
-    int memo[101][101];
+    int memo[103][103];
     int helper(vector <int>& cuts, int l, int r)
     {
-        if(l > r)
+        if(r-l <= 1)
             return 0;
         
         if(memo[l][r] != -1) return memo[l][r];
@@ -12,37 +12,40 @@ public:
         int ans = 1e9;
         int cost = 0;
         
-        if(l==0)
-        {
-            cost += cuts[l];
-        }
-        else
-        {
-            cost += cuts[l] - cuts[l-1];
-        }
+//         if(l==0)
+//         {
+//             cost += cuts[l];
+//         }
+//         else
+//         {
+//             cost += cuts[l] - cuts[l-1];
+//         }
         
-        if(r == cuts.size()-1)
-        {
-            cost += n - cuts[r];
-        }
-        else
-        {
-            cost += cuts[r+1] - cuts[r];
-        }
+//         if(r == cuts.size()-1)
+//         {
+//             cost += n - cuts[r];
+//         }
+//         else
+//         {
+//             cost += cuts[r+1] - cuts[r];
+//         }
         
         cost += cuts[r] - cuts[l];
         
-        for(int i = l; i <= r; i++)
+        for(int i = l+1; i < r; i++)
         {
-            ans = min(ans, cost + helper(cuts,l,i-1) + helper(cuts,i+1,r));
+            ans = min(ans, cost + helper(cuts,l,i) + helper(cuts,i,r));
         }
         
         return memo[l][r] = ans;
     }
     int minCost(int _n, vector<int>& cuts) {
         n = _n;
-        int sz = cuts.size();
+        
         memset(memo,-1,sizeof memo);
+        cuts.push_back(0);
+        cuts.push_back(n);
+        int sz = cuts.size();
         sort(cuts.begin(), cuts.end());
         
         int ans = helper(cuts,0,sz-1);
