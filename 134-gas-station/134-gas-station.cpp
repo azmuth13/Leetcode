9a2cca3876c78cd2f828d734e6252e7437bc5dc6
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-07-07 12:59:22 +0530

    Time: 149 ms (27.22%), Space: 69.4 MB (89.55%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -3,46 +3,56 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n = gas.size();
         
-        gas.insert(gas.end(),gas.begin(),gas.end());
-        cost.insert(cost.end(),cost.begin(),cost.end());
-        int sz = gas.size();
-        vector <int> v(sz+1,-1e9);
-        int pre = 0;
+//         gas.insert(gas.end(),gas.begin(),gas.end());
+//         cost.insert(cost.end(),cost.begin(),cost.end());
         
-        for(int i = 0; i < gas.size(); i++)
-        {
-            if(pre < 0)
-                pre = 0;
-            pre += gas[i];
-            pre -= cost[i];
-            
-            v[i] = pre;
-        }
+//         int sz = gas.size();
+//         vector <int> v(sz+1,-1e9);
+//         int pre = 0;
         
-        int i = 0;
-        while(i < sz)
-        {
-            int j = i;
-            int cnt = 0;
-            while(i < sz && v[i] >= 0)
-            {
-                i++;
-                cnt++;
-            }
-            
-            if(cnt >= n)
-            {
-                return j;
-            }
-            i++;
-        }
-        
-//         for(int i = 0; i < sz; i++)
+//         for(int i = 0; i < gas.size(); i++)
 //         {
-//             cout << v[i] << " ";
+//             if(pre < 0)
+//                 pre = 0;
+//             pre += gas[i];
+//             pre -= cost[i];
+            
+//             v[i] = pre;
 //         }
-//         cout << endl;
         
+//         int i = 0;
+//         while(i < sz)
+//         {
+//             int j = i;
+//             int cnt = 0;
+//             while(i < sz && v[i] >= 0)
+//             {
+//                 i++;
+//                 cnt++;
+//             }
+            
+//             if(cnt >= n)
+//             {
+//                 return j;
+//             }
+//             i++;
+//         }
+
+//         return -1;
+        int ts = 0;
+        int start = 0;
+        int s = 0;
+        for(int i = 0; i < n ; i++)
+        {
+            ts += gas[i]-cost[i];
+            s += gas[i]-cost[i];
+            if(s<0)
+            {
+                s = 0;
+                start= i+1;
+            }
+        }
+        if(ts >=0) return start;
         return -1;
     }
 };
