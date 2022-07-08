ef560d9423885b6832a4bf9043607c6d979fce7b
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-07-08 16:10:35 +0530

    Time: 9 ms (18.22%), Space: 8.6 MB (42.24%) - LeetHub

--- 274-h-index/274-h-index.cpp
+++ 274-h-index/274-h-index.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
+    
+    bool check(int h, vector <int> &c)
+    {
+        int n = c.size();
+        int val=0;
+        for(int i = 0; i < n; i++)
+        {
+            if(c[i] >= h)
+                val++;
+        }
+        return (val >= h);
+    }
+    
     int hIndex(vector<int>& c) {
         
         // k1 + k2 - 1 = c.size()
         // update
         int n = c.size();
-        if(n==1)
-        {
-            return (c[0] !=0);
-        }
+        
         int ans = 0;
         
-        for(int h = 0; h <= n; h++)
-        {
-            int k1 = 0, k2 = 0, k3 = 0;
-            
-            for(int j = 0; j < n; j++)
-            {
-                if(c[j] > h)
-                    k1++;
-                else if(c[j] < h)
-                    k2++;
-                else
-                    k3++;
-            }
-            
-            if(k1+k2+k3 == n && k1+k3 >= h)
-                ans = h;
-            
-        }
+        int l = 0, r = n;
         
+        while(l <= r)
+        {
+            int h = (l+r)/2;
+            
+            if(check(h,c))
+            {
+                ans = h;
+                l = h+1;
+            }
+            else
+            {
+                r = h-1;
+            }
+        }
         return ans;
     }
 };
