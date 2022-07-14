f8a9962d08f97ec382c039f08247e2ade5cc696d
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-07-15 00:54:24 +0530

    Time: 588 ms (32.72%), Space: 6.5 MB (80.51%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -3,7 +3,6 @@ public:
     
     bool isPal(string &s, int i, int j)
     {
-        
         while(i < j)
         {
             
@@ -45,31 +44,66 @@ public:
         return memo[i] = ans;
     }
     
-    int minCut(string s) {
-        int n = s.size();
-        
-        
-        // bool ** storePal = new bool*[n+1];
-        // for(int i=0; i <= n; i++)
-        // {
-        //     storePal[i] = new bool[n+1];
-        //     for(int j = 0; j <= n; j++)
-        //     {
-        //         storePal[i][j] = 0;
-        //     }
-        // }
-        
-        if(isPal(s,0,n-1))
-            return 0;
-        
-        int * memo = new int[n+1];
-        for(int i = 0; i <= n; i++)
+    bool pal(string &s, int i, int j)
+{
+    while(i < j)
+    {
+        if(s[i] != s[j])
+        return false;
+        i++,j--;
+    }
+    return true;
+}
+int memo[2001];
+int helper(string &s, int i)
+{
+    if(i >= s.size())
+    {
+        return 0;
+    }
+    if(memo[i] != -1)
+    return memo[i];
+    int n = s.size(); 
+    int ans = 1e9;
+    for(int j = i; j < n; j++)
+    {
+        if(pal(s,i,j))
         {
-            memo[i] = -1;
+            ans = min(ans, 1 + helper(s,j+1));
         }
+    }
+    return memo[i] =  ans;
+}
+    
+    int minCut(string s) {
+//         int n = s.size();
         
-        int ans = helper(s, 0, memo);
-        return ans;
+        
+//         // bool ** storePal = new bool*[n+1];
+//         // for(int i=0; i <= n; i++)
+//         // {
+//         //     storePal[i] = new bool[n+1];
+//         //     for(int j = 0; j <= n; j++)
+//         //     {
+//         //         storePal[i][j] = 0;
+//         //     }
+//         // }
+        
+//         if(isPal(s,0,n-1))
+//             return 0;
+        
+//         int * memo = new int[n+1];
+//         for(int i = 0; i <= n; i++)
+//         {
+//             memo[i] = -1;
+//         }
+        
+//         int ans = helper(s, 0, memo);
+//         return ans;
+        
+        memset(memo,-1,sizeof memo);
+        int ans = helper(s, 0);
+        return ans-1;
         
     }
 };
