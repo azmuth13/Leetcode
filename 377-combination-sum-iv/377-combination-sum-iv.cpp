7b06048137f8868aa0d85c326eed0b6413720ed0
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-08-05 18:13:43 +0530

    Time: 5 ms (33.70%), Space: 7.2 MB (9.25%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
-    
-//     int helper(vector <int> &coins, int n, int x, int i)
-//     {
-//         if(x == 0) return 1;
-
-//         if(i >= n || x < 0) return 0;
-//         int ans1 = 0, ans2 = 0;
-//         ans1 = helper(coins, n, x - coins[i], i);
-//         ans2 = helper(coins, n, x, i+1);
-//         return (ans1 + ans2);
-//     }
-    
-int dp[1000001];
- 
-int helper(vector <int> &coins, int n, int x)
-{
- 
-    if(x==0)
-        return 1;
- 
-    if(dp[x] != -1)
-        return dp[x];
- 
-    int ans = 0;
-    for(int i = 0; i < n; i++)
+    int memo[200005];
+    int helper(vector<int>& nums, int target)
     {
-        if(coins[i] > x)
-            break;
- 
-        ans += helper(coins, n, x - coins[i]);
+        if(target==0)
+            return 1;
+        
+        if(target < 0) return 0;
+        
+        if(memo[target] != -1) return memo[target];
+        
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            ans += helper(nums, target-nums[i]);
+        }
+        return memo[target] = ans;
     }
- 
-    return dp[x] = ans;
-}
- 
     
     int combinationSum4(vector<int>& nums, int target) {
         
-        int n = nums.size();
-        for(int i = 0; i <= target; i++)
-        {
-            dp[i] = -1;
-        }
-        sort(nums.begin(), nums.end());
-        int ans = helper(nums, n, target);
+        memset(memo,-1,sizeof memo);
+        int ans = helper(nums, target);
         return ans;
     }
 };
