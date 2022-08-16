01f188c455316df94df57bbf067c45194e663e7d
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-08-16 13:25:50 +0530

    Time: 287 ms (53.09%), Space: 21.2 MB (16.60%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int memo[160001][17];
+    bool helper(vector <int> &nums, int mask, int &each, int currSum, int k)
+    {
+        
+        if(k == 1)
+            return true;
+        
+        if(currSum == 0)
+        {
+            return memo[mask][k] = helper(nums, mask, each, each, k-1);
+        }
+        
+        if(memo[mask][k] != -1)
+            return memo[mask][k];
+        
+        for(int j = 0; j<nums.size(); j++)
+        {
+            if((mask & (1<<j)) || currSum - nums[j] < 0)
+                continue;
+
+            if(helper(nums, mask|(1<<j), each, currSum - nums[j], k))
+                return memo[mask][k] = true;
+        }
+        
+        return memo[mask][k] = false;
+    }
+    
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        
+        int sum = 0;
+        
+        for(int i = 0; i<n; i++)
+        {
+            sum += nums[i];
+        }
+        
+        if(sum % k || n < k)
+        {
+            return false;
+        }
+        
+        int each = sum/k;
+        
+        // visited + if not possible then backtrack and explore other 
+        // options
+        
+        // vector <int> vis(n+1, 0);
+        int mask = 0;
+        int currSum = each;
+        memset(memo,-1,sizeof memo);
+        bool ans = helper(nums, mask, each, currSum, k);
+        
+        return ans;
+    }
+};
