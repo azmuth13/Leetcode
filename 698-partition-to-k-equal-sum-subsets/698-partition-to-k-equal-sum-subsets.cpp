ec7adb054b6e072281732666a36f6bf644101432
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-08-16 13:37:31 +0530

    Time: 171 ms (61.27%), Space: 21.3 MB (16.60%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int memo[160001][17];
-    bool helper(vector <int> &nums, int mask, int &each, int currSum, int k)
+    bool helper(vector <int> &nums, int mask, int &each, int currSum, int i, int k)
     {
         
         if(k == 1)
@@ -9,18 +9,18 @@ public:
         
         if(currSum == 0)
         {
-            return memo[mask][k] = helper(nums, mask, each, each, k-1);
+            return memo[mask][k] = helper(nums, mask, each, each, 0, k-1);
         }
         
         if(memo[mask][k] != -1)
             return memo[mask][k];
         
-        for(int j = 0; j<nums.size(); j++)
+        for(int j = i; j<nums.size(); j++)
         {
             if((mask & (1<<j)) || currSum - nums[j] < 0)
                 continue;
 
-            if(helper(nums, mask|(1<<j), each, currSum - nums[j], k))
+            if(helper(nums, mask|(1<<j), each, currSum - nums[j], j+1, k))
                 return memo[mask][k] = true;
         }
         
@@ -52,7 +52,7 @@ public:
         int mask = 0;
         int currSum = each;
         memset(memo,-1,sizeof memo);
-        bool ans = helper(nums, mask, each, currSum, k);
+        bool ans = helper(nums, mask, each, currSum, 0, k);
         
         return ans;
     }
