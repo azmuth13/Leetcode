92099e335e76e3440e564e476c0eb81d08ee4202
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-08-20 17:18:44 +0530

    Time: 26 ms (97.89%), Space: 12.9 MB (77.95%) - LeetHub

---
+++ 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
@@ -0,0 +1,27 @@
+
+long long dp[1001][1001] = {};
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int helper(int n, int k)
+    {
+        if(n==k)
+            return 1;
+        if(k==0)
+            return 0;
+        if(dp[n][k] != 0)
+            return dp[n][k];
+        
+        dp[n][k] += helper(n-1,k-1);
+        dp[n][k]%=mod;
+        
+        dp[n][k] += (1ll*(n-1)*helper(n-1,k)%mod)%mod;
+        dp[n][k]%=mod;
+        return dp[n][k];
+        
+    }
+    int rearrangeSticks(int n, int k) {
+        int ans = helper(n,k);
+        return ans;
+    }
+};
