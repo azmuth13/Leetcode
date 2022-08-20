c671a7a88154e08a9380f0957803523b16ee1c6e
SURAJ KUMAR <72400473+azmuth13@users.noreply.github.com>
2022-08-20 18:18:24 +0530

    Time: 489 ms (5.04%), Space: 25.2 MB (17.61%) - LeetHub

---
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int uniqueLetterString(string s) {
+        int n = s.size();
+        map <char, vector <int> > mp;
+        for(char ch = 'A'; ch <= 'Z'; ch++)
+        {
+            mp[ch].push_back(-1e9);
+        }
+        for(int i = 0; i < n; i++)
+        {
+            mp[s[i]].push_back(i);
+        }
+        for(char ch = 'A'; ch <= 'Z'; ch++)
+        {
+            mp[ch].push_back(1e9);
+        }
+        int ans = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            auto it = lower_bound(mp[s[i]].begin(), mp[s[i]].end(), i+1);
+            int right, left;
+            
+            if(it == mp[s[i]].end())
+            {
+                cout << "YES\n";
+                return 0;
+            }
+         
+            if(*it == 1e9)
+                right = n;
+            else
+                right = *it;
+            
+            it = lower_bound(mp[s[i]].begin(), mp[s[i]].end(), i);
+            
+//             if(it == mp[s[i]].begin())
+//             {
+//                 cout << "YES" << ' ' << i << ' ' << s[i] << endl;
+//                 return 0;
+//             }
+            
+            it--;
+            
+            if(*it == -1e9)
+                left = -1;
+            else
+                left = *it;
+            int lenL = i-left;
+            int lenR = right-i;
+            ans += lenL*lenR;
+        }
+        return ans;
+    }
+};
